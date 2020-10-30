a41d9b34bfcba1fc33567f47a6e5fe08706971ea
Admin <hudaierbas@gmail.com>
2020-10-30 11:17:00 +0300

    home, product components created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import Header from "./component/Header";
+import Home from "./component/Home";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      {/* Home */}
+      <Home />
     </div>
   );
 }
